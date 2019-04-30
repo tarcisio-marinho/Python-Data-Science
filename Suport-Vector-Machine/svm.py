f32ce15c43abcc9fb96eab7f11403395f8ffef35
tarcisio-marinho <tarcisio_marinho09@hotmail.com>
2019-04-30 00:58:46 -0300

    fixing python script

--- Suport-Vector-Machine/svm.py
+++ Suport-Vector-Machine/svm.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors, svm
+from sklearn import preprocessing, model_selection, neighbors, svm
 import pandas as pd
 
 
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     x = np.array(df.drop(['class'], 1))
     y = np.array(df['class'])
 
-    x_train, x_test, y_train, y_test = cross_validation.train_test_split(x, y, test_size=0.2) # teste de treino
+    x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2) # teste de treino
     clf = svm.SVC() # instancia o classificador
     clf.fit(x_train, y_train) # treinar o classificador
 
